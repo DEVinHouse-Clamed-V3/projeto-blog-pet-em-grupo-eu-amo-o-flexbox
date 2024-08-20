b78a95df89e30ab61abe743be69fe1e0b994ccb3
Lucas da Silva <sklucassilva@gmail.com>
2024-08-20 16:40:10 -0300

    feat: Add class to "Load Posts" button in listagem_post_admin.html

--- scripts/listagem_post_admin.js
+++ scripts/listagem_post_admin.js
@@ -1,5 +1,3 @@
-function loadPosts() {}
-
 function tempoLeitura(post) {
   const descricaoPost = post.descricao;
   const tempoLeitura = descricaoPost.length / 200;
@@ -9,6 +7,37 @@ function tempoLeitura(post) {
   } else {
     return `${Math.ceil(tempoLeitura)} min`; // Arredonda para cima o valor do tempo de leitura.
   }
+}
+
+function loadPosts() {
+  const listaPosts = document.querySelector('.filme-posts');
+  const posts = JSON.parse(localStorage.getItem('posts')) || [];
+
+  //listaPosts.innerHTML = '';
+
+  posts.forEach((post, index) => {
+    const postElement = `
+      <a href="" class="filme-card">
+        <div class="image">
+          <img src="images/${post.imagem}" alt="${post.titulo}">
+        </div>
+        <div class="info">
+          <div class="top-info">
+            <span>⭐ ${post.nota}</span>
+          </div>
+          <h3>${post.titulo}</h3>
+          <span>${post.data} - ${tempoLeitura(post)} read</span>
+        </div>
+      </a>
+    `;
+
+    listaPosts.appendChild(postElement);
+  });
+}
+
+function loadMorePosts() {
+  const btnLoadMore = document.querySelector('.btn-load-more');
+  btnLoadMore.addEventListener('click', loadPosts);
 }
 
 document.addEventListener('DOMContentLoaded', loadPosts);
