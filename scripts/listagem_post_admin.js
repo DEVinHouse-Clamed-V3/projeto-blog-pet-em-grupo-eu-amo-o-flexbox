be7861b90c750610027238fb65a57c091d4c5f91
Lucas da Silva <sklucassilva@gmail.com>
2024-08-20 16:25:26 -0300

    refactor: Add function to calculate reading time for posts

--- scripts/listagem_post_admin.js
+++ scripts/listagem_post_admin.js
@@ -1,5 +1,14 @@
-function loadPosts() {
-  
+function loadPosts() {}
+
+function tempoLeitura(post) {
+  const descricaoPost = post.descricao;
+  const tempoLeitura = descricaoPost.length / 200;
+
+  if (tempoLeitura <= 1) {
+    return '1 min';
+  } else {
+    return `${Math.ceil(tempoLeitura)} min`; // Arredonda para cima o valor do tempo de leitura.
+  }
 }
 
 document.addEventListener('DOMContentLoaded', loadPosts);
