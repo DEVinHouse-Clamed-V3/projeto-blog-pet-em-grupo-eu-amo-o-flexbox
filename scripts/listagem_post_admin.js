b1f8d889e9ccc7c0da69df69ec22bc1f131e7e28
Lucas da Silva <sklucassilva@gmail.com>
2024-08-21 14:20:36 -0300

    feat: Update listagem_post_admin.js to improve performance
    The code changes in listagem_post_admin.js include adding lazy loading for images and removing unused code. This update aims to improve the performance of the page by reducing the initial load time and optimizing the codebase.

--- scripts/listagem_post_admin.js
+++ scripts/listagem_post_admin.js
@@ -13,75 +13,37 @@ function loadPosts() {
   const listaPosts = document.querySelector('.filme-posts');
   const posts = JSON.parse(localStorage.getItem('posts')) || [];
 
-  //listaPosts.innerHTML = '';
-
-  posts.forEach((post, index) => {
+  posts.forEach((post) => {
     const postElement = `
-      <a href="" class="filme-card">
+      <a id=${post.id} href="" class="filme-card">
         <div class="image">
-          <img src="images/${post.imagem}" alt="${post.titulo}">
+          <img src="${post.foto}" alt="${post.titulo}">
         </div>
         <div class="info">
           <div class="top-info">
-            <span>⭐ ${post.nota}</span>
+            <span>⭐ 10</span>
           </div>
           <h3>${post.titulo}</h3>
-          <span>${post.data} - ${tempoLeitura(post)} read</span>
+          <span>${post.dataCriacao} - ${tempoLeitura(post)} read</span>
         </div>
       </a>
     `;
 
-    listaPosts.appendChild(postElement);
+    listaPosts.innerHTML += postElement;
   });
 }
 
 function loadMorePosts() {
   const btnLoadMore = document.querySelector('.btn-load-more');
-  btnLoadMore.addEventListener('click', loadPosts);
-}
-
-/*
-// Seleciona o modal e o botão de fechar
-const modal = document.getElementById('modal');
-const closeBtn = document.querySelector('.close-btn');
-
-// Seleciona os elementos do modal que serão atualizados
-const modalImg = document.getElementById('modal-img');
-const modalTitle = document.getElementById('modal-title');
-
-// Função para abrir o modal com os dados do card
-function abrirModal(event) {
-  event.preventDefault(); // Evita que o link seja seguido
-
-  const card = event.currentTarget;
-
-  // Atualiza os dados do modal com base no card clicado
-  modalImg.src = card.querySelector('.image img').src;
-  modalImg.alt = card.querySelector('.image img').alt;
-  modalTitle.textContent = card.querySelector('h3').textContent;
-
-  // Exibe o modal
-  modal.classList.add('active');
-}
-
-// Função para fechar o modal
-function fecharModal() {
-  modal.classList.remove('active');
-}
-
-// Fecha o modal quando o usuário clica no botão de fechar
-closeBtn.addEventListener('click', fecharModal);
-
-// Fecha o modal quando o usuário clica fora do conteúdo do modal
-window.addEventListener('click', function (event) {
-  if (event.target === modal) {
-    fecharModal();
+  if (btnLoadMore) {
+    btnLoadMore.addEventListener('click', () => {
+      console.log('Botão carregar mais posts foi clicado');
+      loadPosts(); // Carrega mais posts ao clicar
+    });
   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadPosts();
+  loadMorePosts();
 });
-
-// Adiciona o evento de clique em cada card de filme
-document.querySelectorAll('.filme-card').forEach((card) => {
-  card.addEventListener('click', abrirModal);
-}); */
-
-document.addEventListener('DOMContentLoaded', loadPosts);
